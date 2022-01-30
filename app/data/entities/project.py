966673734c5ea5ca742f2e49c4bc15641be6cc89
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-01-30 22:20:53 +0100

    :card_file_box: Add projects database entity

---
+++ app/data/entities/project.py
@@ -0,0 +1,15 @@
+# Created by Kelvin_Clark on 1/30/2022, 10:15 PM
+import sqlalchemy as sa
+
+from app.data import SQLALCHEMY_BASE
+
+
+class Project(SQLALCHEMY_BASE):
+    __tablename__ = "projects"
+    id: int = sa.Column(sa.BIGINT, primary_key=True, autoincrement=True)
+    name: str = sa.Column(sa.String, nullable=False)
+    description: str = sa.Column(sa.TEXT)
+
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
