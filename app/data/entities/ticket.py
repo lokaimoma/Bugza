e58b4258427d0c303e63866e2bef413a905bcfcd
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-01-30 22:31:45 +0100

    :card_file_box: Add tickets database entity

---
+++ app/data/entities/ticket.py
@@ -0,0 +1,23 @@
+# Created by Kelvin_Clark on 1/30/2022, 10:21 PM
+import sqlalchemy as sa
+
+from app.data import SQLALCHEMY_BASE
+from app.data.enum.ticket_type import TicketType
+
+
+class Ticket(SQLALCHEMY_BASE):
+    __tablename__ = "tickets"
+    project_id: int = sa.Column(sa.BIGINT, sa.ForeignKey("projects.id", ondelete="CASCADE", onupdate="CASCADE"),
+                                primary_key=True)
+    opener_id: int = sa.Column(sa.BIGINT, sa.ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"),
+                               primary_key=True)
+    title: str = sa.Column(sa.String, nullable=False)
+    description: str = sa.Column(sa.TEXT)
+    type: TicketType = sa.Column(sa.Enum(TicketType.ISSUE, TicketType.FEATURE_REQUEST), nullable=False)
+
+    def __init__(self, project_id: int, opener_id: int, title: str, description: str, type: TicketType):
+        self.project_id = project_id
+        self.opener_id = opener_id
+        self.title = title
+        self.description = description
+        self.type = type
---
+++ app/data/enum/ticket_type.py
@@ -0,0 +1,8 @@
+# Created by Kelvin_Clark on 1/30/2022, 10:27 PM
+from enum import StrEnum
+
+
+class TicketType(StrEnum):
+    ISSUE = "issue"
+    FEATURE_REQUEST = "feature_request"
+
