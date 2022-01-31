cc9db43ba7b996418576c1a8f32179b9a2733f20
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-01-31 12:14:50 +0100

    :building_construction: Add user pydantic models

---
+++ app/api/__init__.py
@@ -0,0 +1,1 @@
+# Created by Kelvin_Clark on 1/31/2022, 10:30 AM
---
+++ app/data/schema/pydantic/user.py
@@ -0,0 +1,28 @@
+# Created by Kelvin_Clark on 1/31/2022, 12:10 PM
+from typing import Optional
+
+from pydantic import BaseModel
+
+from app.data.enum.roles import Role
+
+
+class UserIn(BaseModel):
+    username: str
+    email: str
+    password: str
+    role: Optional[Role] = Role.TESTER
+
+    class Config:
+        orm_mode = True
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    role: Role
+    token: Optional[str]
+    toke_type: Optional[str]
+
+    class Config:
+        orm_mode = True
