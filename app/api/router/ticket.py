df5672f3e968ce275f492be42ae3526a022e4264
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-01 23:06:48 +0100

    Add create comment route

--- app/api/router/ticket.py
+++ app/api/router/ticket.py
@@ -5,8 +5,10 @@ from starlette.status import HTTP_201_CREATED
 
 from app.api.dependencies.oauth import get_current_user
 from app.data import get_sync_session
+from app.data.schema.pydantic.comment import CommentOut, CommentIn
 from app.data.schema.pydantic.ticket import TicketOut, TicketIn
 from app.data.schema.pydantic.user import UserOut
+from app.data.usecases.insert.insert_comment import insert_comment
 from app.data.usecases.insert.insert_ticket import insert_ticket
 
 router = APIRouter(prefix="/ticket", tags=["Tickets"])
@@ -17,3 +19,10 @@ async def create_ticket(ticket: TicketIn, _: UserOut = Depends(get_current_user)
                         session: Session = Depends(get_sync_session)):
     ticket = insert_ticket(session=session, ticket=ticket)
     return ticket
+
+
+@router.post(path="/comment", status_code=HTTP_201_CREATED, response_model=CommentOut)
+async def create_comment(comment: CommentIn, session: Session = Depends(get_sync_session),
+                         _: UserOut = Depends(get_current_user)):
+    comment = insert_comment(comment=comment, session=session)
+    return comment
