6da7196a313ced57ada2ae8bd442de5db593c36e
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-01 12:48:18 +0100

    Implement create ticket route

---
+++ app/api/router/ticket.py
@@ -0,0 +1,19 @@
+# Created by Kelvin_Clark on 2/1/2022, 12:24 PM
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_201_CREATED
+
+from app.api.dependencies.oauth import get_current_user
+from app.data import get_sync_session
+from app.data.schema.pydantic.ticket import TicketOut, TicketIn
+from app.data.schema.pydantic.user import UserOut
+from app.data.usecases.insert.insert_ticket import insert_ticket
+
+router = APIRouter(prefix="/ticket", tags=["Tickets"])
+
+
+@router.post(path="/create", status_code=HTTP_201_CREATED, response_model=TicketOut)
+async def create_ticket(ticket: TicketIn, _: UserOut = Depends(get_current_user),
+                        session: Session = Depends(get_sync_session)):
+    ticket = insert_ticket(session=session, ticket=ticket)
+    return ticket
--- app/data/entities/ticket.py
+++ app/data/entities/ticket.py
@@ -10,16 +10,16 @@ class Ticket(SQLALCHEMY_BASE):
     __tablename__ = "tickets"
     id: int = sa.Column(sa.BIGINT().with_variant(sa.Integer, "sqlite"), primary_key=True, autoincrement=True)
     project_id: int = sa.Column(sa.BIGINT, sa.ForeignKey("projects.id", ondelete="CASCADE", onupdate="CASCADE"))
-    opener_id: int = sa.Column(sa.BIGINT, sa.ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"))
+    creator_id: int = sa.Column(sa.BIGINT, sa.ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"))
     title: str = sa.Column(sa.String(255), nullable=False)
     description: str = sa.Column(sa.TEXT)
     type: TicketType = sa.Column(sa.Enum(TicketType), nullable=False)
     state: TicketState = sa.Column(sa.Enum(TicketState), nullable=False)
 
-    def __init__(self, project_id: int, opener_id: int, title: str, description: str, ticket_type: TicketType,
+    def __init__(self, project_id: int, creator_id: int, title: str, description: str, ticket_type: TicketType,
                  ticket_state: TicketState):
         self.project_id = project_id
-        self.opener_id = opener_id
+        self.creator_id = creator_id
         self.title = title
         self.description = description
         self.type = ticket_type
