8591778ee61207bd7ccb9a94cc2289664b35ef0b
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-01 23:00:28 +0100

    Add pydantic models for comments

---
+++ app/data/schema/pydantic/comment.py
@@ -0,0 +1,15 @@
+# Created by Kelvin_Clark on 2/1/2022, 10:58 PM
+from pydantic import BaseModel
+
+
+class CommentIn(BaseModel):
+    user_id: int
+    ticket_id: int
+    text: str
+
+    class Config:
+        orm_mode = True
+
+
+class CommentOut(CommentIn):
+    id: int
