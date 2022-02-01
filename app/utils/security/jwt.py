e49d464a305f88fbfbc54dfcce7fe80718d01c98
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-01 14:36:40 +0100

    Fix bug in jwt model
    During the token generation phase I placed user data into another dict
    assigning the data to a key of 'sub'. During the phase of validation and
    getting of data I was having error, searched about it but didn't find
    any way around it to removed that key.

--- app/utils/security/jwt.py
+++ app/utils/security/jwt.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ[SECRET_KEY_ENV_KEY]
 
 
 def create_access_token(data: dict) -> str:
-    data_ = {"sub": data.copy()}
+    data_ = data.copy()
     expiry_time = datetime.utcnow() + timedelta(seconds=ACCESS_TOKEN_EXPIRE_SECONDS)
     data_.update({"exp": expiry_time})
     token = jwt.encode(data_, key=SECRET_KEY, algorithm=ALGORITHM)
@@ -24,21 +24,13 @@ def get_token_data(token: str) -> Union[dict, bool]:
     """
     Returns the content of a JWT string if the token is valid
     otherwise returns a bool of value False.
-    A value error exception will be raised if the token contains
-    no data (i.e: The token dict has no entry for the 'sub' key)
     :param token: JWT token string
     :return: Token content if token is valid.
     """
     try:
-        data = __verify_token(token=token)
-        return data
+        return jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
         return False
 
 
-def __verify_token(token: str) -> dict:
-    payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
-    data = payload.get("sub")
-    if data is None:
-        raise ValueError("The token passed doesn't contain any data")
-    return data
+
