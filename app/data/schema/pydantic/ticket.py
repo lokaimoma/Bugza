c09e6623b7cf3555048cbf4d777341fc0bb8d081
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-01 12:36:08 +0100

    Add pydantic ticket models

---
+++ app/data/schema/pydantic/ticket.py
@@ -0,0 +1,22 @@
+# Created by Kelvin_Clark on 2/1/2022, 12:28 PM
+from pydantic import BaseModel
+
+from app.data.enum.ticket_state import TicketState
+from app.data.enum.ticket_type import TicketType
+
+
+class TicketIn(BaseModel):
+    project_id: int
+    creator_id: int
+    title: str
+    description: str
+    type: TicketType
+    state: TicketState
+
+    class Config:
+        orm_mode = True
+
+
+class TicketOut(TicketIn):
+    id: int
+
