d0079f8f4a87f6f912478311d9012452bd663a5f
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-02 12:58:35 +0100

    Test tickets and ticket graphql endpoints

--- tests/test_graphql_router.py
+++ tests/test_graphql_router.py
@@ -106,3 +106,127 @@ async def test_get_project_by_id_(project, graphql_app):
         assert "project" in json["data"]
         assert json["data"]["project"] is not None
         assert json["data"]["project"]["id"] == project.id
+
+
+@pytest.mark.anyio
+async def test_get_all_tickets_no_ticket(graphql_app):
+    async with AsyncClient(app=graphql_app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        query = """
+            query GET_TICKETS {
+                tickets {
+                    id
+                    projectId
+                    creatorId
+                    title
+                    description
+                    type
+                    state
+                }
+            }
+        """
+        request_body = {
+            "query": query,
+            "operationName": "GET_TICKETS",
+            "variables": {}
+        }
+        response = await ac.post(url=TEST_GRAPHQL_PATH, json=request_body)
+        assert response.status_code == HTTP_200_OK
+        json = response.json()
+        assert "data" in json
+        assert "tickets" in json["data"]
+        assert len(json["data"]["tickets"]) == 0
+
+
+@pytest.mark.anyio
+async def test_get_all_tickets(ticket_user, graphql_app):
+    ticket = ticket_user[0]
+    async with AsyncClient(app=graphql_app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        query = """
+            query GET_TICKETS {
+                tickets {
+                    id
+                    projectId
+                    creatorId
+                    title
+                    description
+                    type
+                    state
+                }
+            }
+        """
+        request_body = {
+            "query": query,
+            "operationName": "GET_TICKETS",
+            "variables": {}
+        }
+        response = await ac.post(url=TEST_GRAPHQL_PATH, json=request_body)
+        assert response.status_code == HTTP_200_OK
+        json = response.json()
+        assert "data" in json
+        assert "tickets" in json["data"]
+        assert len(json["data"]["tickets"]) == 1
+        assert json["data"]["tickets"][0]["id"] == ticket.id
+
+
+@pytest.mark.anyio
+async def test_get_ticket_by_id_no_ticket(graphql_app):
+    async with AsyncClient(app=graphql_app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        query = """
+            query GET_TICKET($ticket_id: Int!) {
+                ticket(ticketId: $ticket_id) {
+                    id
+                    projectId
+                    creatorId
+                    title
+                    description
+                    type
+                    state
+                }
+            }
+        """
+        request_body = {
+            "query": query,
+            "operationName": "GET_TICKET",
+            "variables": {"ticket_id": 1}
+        }
+        response = await ac.post(url=TEST_GRAPHQL_PATH, json=request_body)
+        assert response.status_code == HTTP_200_OK
+        json = response.json()
+        assert "data" in json
+        assert "ticket" in json["data"]
+        assert json["data"]["ticket"] is None
+
+
+@pytest.mark.anyio
+async def test_get_ticket_by_id(ticket_user, graphql_app):
+    ticket = ticket_user[0]
+    async with AsyncClient(app=graphql_app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        query = """
+            query GET_TICKET($ticket_id: Int!) {
+                ticket(ticketId: $ticket_id) {
+                    id
+                    projectId
+                    creatorId
+                    title
+                    description
+                    type
+                    state
+                }
+            }
+        """
+        request_body = {
+            "query": query,
+            "operationName": "GET_TICKET",
+            "variables": {"ticket_id": ticket.id}
+        }
+        response = await ac.post(url=TEST_GRAPHQL_PATH, json=request_body)
+        assert response.status_code == HTTP_200_OK
+        json = response.json()
+        assert "data" in json
+        assert "ticket" in json["data"]
+        assert json["data"]["ticket"] is not None
+        assert json["data"]["ticket"]["id"] == ticket.id
