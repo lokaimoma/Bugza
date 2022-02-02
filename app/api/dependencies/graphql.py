2aa5dfd137e8cb0d822f44147e7f647129df5a87
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-02 02:56:56 +0100

    Add strawberry database session extensions

---
+++ app/api/dependencies/graphql.py
@@ -0,0 +1,15 @@
+# Created by Kelvin_Clark on 2/2/2022, 2:47 AM
+from strawberry.extensions import Extension
+
+from app.utils.constants import DB_SESSION_ASYNC, DB_SESSION_SYNC
+from app.data import get_async_session_unmanaged,get_sync_session_unmanaged
+
+
+class SQLAlchemySessionExtension(Extension):
+    def on_request_start(self):
+        self.execution_context.context[DB_SESSION_SYNC] = get_sync_session_unmanaged()
+        self.execution_context.context[DB_SESSION_ASYNC] = get_async_session_unmanaged()
+
+    async def on_request_end(self):
+        self.execution_context.context[DB_SESSION_SYNC].close()
+        await self.execution_context.context[DB_SESSION_ASYNC].close()
