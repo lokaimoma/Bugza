3b80218b3eb038af11e9d6fa4f2f515c093315cd
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-07 15:37:28 +0100

    Enable cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from typing import Optional
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 __app: Optional[FastAPI] = None
+origins = [
+    "http://localhost:3000",
+]
 
 
 def create_app() -> FastAPI:
@@ -14,6 +18,12 @@ def create_app() -> FastAPI:
     if __app is not None:
         return __app
     __app = FastAPI(title="Bugza", description="Backend System of Bugza - A bug tracking system")
+    __app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"])
     from app.api.router.auth import router as auth_router
     from app.api.router.project import router as project_router
     from app.api.router.ticket import router as ticket_router
