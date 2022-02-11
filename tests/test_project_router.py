1cfd4af09bfa70212e65e23fa66c9e5e08a93b2b
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-11 21:31:28 +0100

    Test projects router

--- tests/test_project_router.py
+++ tests/test_project_router.py
@@ -1,7 +1,7 @@
 # Created by Kelvin_Clark on 2/1/2022, 1:53 PM
 import pytest
 from httpx import AsyncClient
-from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_201_CREATED
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_201_CREATED, HTTP_200_OK
 
 from app.utils.constants import TEST_BASE_URL
 
@@ -38,3 +38,63 @@ async def test_insert_project_admin(admin_user, app):
         assert "id" in json
         assert json["id"] is not None
         assert json["name"] == project["name"]
+
+
+@pytest.mark.anyio
+async def test_get_all_projects(ticket_user, app):
+    user = ticket_user[1]
+    async with AsyncClient(app=app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        ac.headers = {**ac.headers, "Authorization": f"Bearer {user['token']}"}
+        response = await ac.get("/project/")
+        json = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert len(json) > 0
+        assert "id" in json[0]
+        assert "description" in json[0]
+        assert "name" in json[0]
+
+
+@pytest.mark.anyio
+async def test_get_latest(ticket_user, app):
+    user = ticket_user[1]
+    async with AsyncClient(app=app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        ac.headers = {**ac.headers, "Authorization": f"Bearer {user['token']}"}
+        response = await ac.get("/project/latest")
+        json = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert len(json) <= 10
+        assert "id" in json[0]
+        assert "description" in json[0]
+        assert "name" in json[0]
+
+
+@pytest.mark.anyio
+async def test_get_project_stats(ticket_user, app):
+    user = ticket_user[1]
+    async with AsyncClient(app=app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        ac.headers = {**ac.headers, "Authorization": f"Bearer {user['token']}"}
+        response = await ac.get(f"/project/stats?project_id={ticket_user[0].project_id}")
+        json = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert "open_issues" in json
+        assert "open_feature_requests" in json
+        assert "closed_tickets" in json
+        assert json["open_issues"] == 1
+
+
+@pytest.mark.anyio
+async def test_get_project_summary(ticket_user, app):
+    user = ticket_user[1]
+    async with AsyncClient(app=app, base_url=TEST_BASE_URL) as ac:
+        ac: AsyncClient
+        ac.headers = {**ac.headers, "Authorization": f"Bearer {user['token']}"}
+        response = await ac.get("/project/summary")
+        json = response.json()
+        assert response.status_code == HTTP_200_OK
+        assert "total_projects" in json
+        assert json["total_projects"] == 1
+        assert json["with_issues"] == 1
+        assert json["without_issues"] == 0
