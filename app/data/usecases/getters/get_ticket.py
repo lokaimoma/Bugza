4f27d8312235cac8b3cbdfeb347bd4c77c8ea658
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-11 10:06:49 +0100

    Fix missing greenlet errors
    When accessing a collection on an entity with
    an async session we have to pass an option to
    eagerly load the collection before we use it
    unlike the synchronous session.
    More on this at https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html#asyncio-orm-avoid-lazyloads

--- app/data/usecases/getters/get_ticket.py
+++ app/data/usecases/getters/get_ticket.py
@@ -2,6 +2,7 @@
 from typing import List, Optional
 
 from sqlalchemy import select, func, and_
+from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.data.entities.ticket import Ticket
@@ -12,19 +13,20 @@ from app.utils.constants import ROWS_PER_PAGE
 
 
 async def get_tickets_by_project_id(session: AsyncSession, project_id: int) -> List[Ticket]:
-    query = select(Ticket).where(Ticket.project_id == project_id)
+    query = select(Ticket).where(Ticket.project_id == project_id).options(selectinload(Ticket.project))
     result = await session.execute(query)
     return result.scalars().all()
 
 
 async def get_tickets(session: AsyncSession, page_number: int) -> List[Ticket]:
-    query = select(Ticket).limit(ROWS_PER_PAGE).offset(ROWS_PER_PAGE * page_number)
+    query = select(Ticket).limit(ROWS_PER_PAGE).offset(ROWS_PER_PAGE * page_number).options(
+        selectinload(Ticket.project))
     result = await session.execute(query)
     return result.scalars().all()
 
 
 async def get_ticket_by_id(session: AsyncSession, ticket_id: int) -> Optional[Ticket]:
-    query = select(Ticket).where(Ticket.id == ticket_id)
+    query = select(Ticket).where(Ticket.id == ticket_id).options(selectinload(Ticket.project))
     result = await session.execute(query)
     return result.scalar_one_or_none()
 
