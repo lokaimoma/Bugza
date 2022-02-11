984f81f94e4b4ca3c66373b2d0d41f23244586b1
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-02-11 20:18:39 +0100

    Get comment user when requested from db

--- app/data/schema/gaphql/comment.py
+++ app/data/schema/gaphql/comment.py
@@ -1,8 +1,13 @@
 # Created by Kelvin_Clark on 2/2/2022, 1:18 AM
+from typing import Optional
+
 import strawberry
+from strawberry.types import Info
 
 from app.data.entities.comments import Comment as CommentModel
 from app.data.schema.gaphql.user import User
+from app.data.usecases.getters.get_user import get_user_by_id
+from app.utils.constants import DB_SESSION_SYNC
 
 
 @strawberry.type
@@ -12,15 +17,15 @@ class Comment:
     ticket_id: int
     text: str
 
-    instance: strawberry.Private[CommentModel]
-
     @strawberry.field
-    def user(self) -> User:
-        return User(user=self.instance.user)
+    def creator(self, info: Info) -> Optional[User]:
+        user = get_user_by_id(session=info.context[DB_SESSION_SYNC], user_id=self.user_id)
+        if user is None:
+            return None
+        return User(user=user)
 
     def __init__(self, comment: CommentModel):
         self.id = comment.id
         self.user_id = comment.user_id
         self.ticket_id = comment.ticket_id
         self.text = comment.text
-        self.instance = comment
